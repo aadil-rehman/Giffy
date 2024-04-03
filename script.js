165e1be73f82368abd6c67b29a5258809f1a2e2a
Moh Aadil <aadilrehman.development@gmail.com>
2024-04-03 16:36:56 +0530

    more changes

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 const body = document.querySelector("body");
 const image = document.querySelector("img");
 
-console.dir(image);
+let prevClickX = 0;
+let isImageFlipped = false;
 
 body.addEventListener("click", (e) => {
 	const { x, y } = e;
+
+	let imageX = image.x;
+	let imageY = image.y;
+
+	if (x > prevClickX) {
+		if (!isImageFlipped) {
+			image.style.transform = "scaleX(-1)";
+			isImageFlipped = true;
+		}
+	} else {
+		if (isImageFlipped) {
+			image.style.transform = "scaleX(1)";
+			isImageFlipped = false;
+		}
+	}
+
+	prevClickX = x;
 
 	image.style.left = `${x - 50}px`;
 	image.style.top = `${y - 50}px`;
